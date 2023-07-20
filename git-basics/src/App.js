bc5d499df8bda11f8482b6d38c02d62e360de7e4
Sakthivel-Vadivel <sakthivellvadivel@gmail.com>
2023-07-20 16:10:58 +0530

    feat:add the main page

--- git-basics/src/App.js
+++ git-basics/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <p>Main page</p>
       <p>
         The version of this<strong> {version.name} </strong>app is{" "}
         <strong>{version.version}</strong>
